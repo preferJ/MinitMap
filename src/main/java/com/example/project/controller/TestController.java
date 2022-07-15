1da8f4b4f8ef453e7e7ee5c7ffc0eb9c8929b00e
preferJ <prefer0415@gmail.com>
2022-07-15 13:55:41 +0900

    test sout 추가

--- src/main/java/com/example/project/controller/TestController.java
+++ src/main/java/com/example/project/controller/TestController.java
@@ -5,8 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TestController {
+//    ㅅㅎ 테스트 sout 추가
     @GetMapping("/")
     public String gps(){
+        System.out.println("테스트");
         return "/gps";
     }
 }

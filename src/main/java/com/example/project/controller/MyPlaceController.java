c556b28f397b3675a93859624148164831659faf
21su <ghdtjdtn00@naver.com>
2022-07-28 16:21:04 +0900

    내가 뭘했더라

---
+++ src/main/java/com/example/project/controller/MyPlaceController.java
@@ -0,0 +1,30 @@
+package com.example.project.controller;
+
+import com.example.project.dto.MyPlaceDTO;
+import com.example.project.service.MyPlaceService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@AllArgsConstructor
+@RequestMapping("/myPlace")
+public class MyPlaceController {
+    private final MyPlaceService myPlaceService;
+
+    @GetMapping("/save")
+    public String save(@RequestParam("myPlaceLat") Double myPlaceLat,
+                       @RequestParam("myPlaceLng") Double myPlaceLng,
+                       @RequestParam("myPlaceName") String myPlaceName,
+                       @RequestParam(value = "icon",required = false,defaultValue = "0") Long icon,
+                       HttpSession session){
+        Long id = (Long) session.getAttribute("loginId");
+        MyPlaceDTO myPlaceDTO = new MyPlaceDTO(id,myPlaceName,myPlaceLat,myPlaceLng,icon);
+        myPlaceService.save(myPlaceDTO);
+        return "hss/tapDown";
+    }
+}
--- src/main/java/com/example/project/repository/MyPlaceRepository.java
+++ src/main/java/com/example/project/repository/MyPlaceRepository.java
@@ -1,7 +1,11 @@
 package com.example.project.repository;
 
+import com.example.project.entity.MemberEntity;
 import com.example.project.entity.MyPlaceEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MyPlaceRepository extends JpaRepository<MyPlaceEntity,Long> {
+    List<MyPlaceEntity> findByMemberEntity(MemberEntity memberEntity);
 }

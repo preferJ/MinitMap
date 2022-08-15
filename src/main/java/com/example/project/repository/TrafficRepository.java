629b1e88bf2f465851dc59e37ff3a1119fd83db0
LeeHyeon2 <sd0315@daum.net>
2022-08-15 19:08:11 +0900

    이현 // 이gus123

--- src/main/java/com/example/project/repository/TrafficRepository.java
+++ src/main/java/com/example/project/repository/TrafficRepository.java
@@ -1,8 +1,10 @@
 package com.example.project.repository;
 
 import com.example.project.entity.MemberEntity;
+import com.example.project.entity.MyTrafficEntity;
 import com.example.project.entity.TrafficEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,4 +18,6 @@ public interface TrafficRepository extends JpaRepository<TrafficEntity,Long> {
     List<TrafficEntity> findByMemberEntityOrderByTrafficDislikeAsc(MemberEntity memberEntity);
     List<TrafficEntity> findByMemberEntityOrderByTrafficDislikeDesc(MemberEntity memberEntity);
 
+    @Query("select t from TrafficEntity t where t.trafficLat between :a and :b and t.trafficLon between :c and :d")
+    List<TrafficEntity> findBetween(Double a , Double b , Double c , Double d);
 }
